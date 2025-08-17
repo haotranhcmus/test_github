382bc11b31d9871318231420c1c6f3e2f869e5bc
CoderLonely <haotranhcmus@gmail.com>
2025-08-17 14:00:13 +0700

    fix

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,1 +1,13 @@
-from . import *
+from .haotran import hao_say_hello
+from .chihieu import hieu_say_hello
+from .quochiep import hiep_say_hello
+from .tbao import tbao_say_hello
+from .thanhhau import thau_say_hello
+
+__all__ = [
+    'hao_say_hello',
+    'hieu_say_hello', 
+    'hiep_say_hello',
+    'tbao_say_hello',
+    'thau_say_hello'
+]    
