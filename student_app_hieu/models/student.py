f3ec39dd5578e3c2875d0a8ebe56bb5e86ff5fe1
Phạm Chí Hiếu <chiiwhiuu@192.168.1.25>
2025-08-21 17:08:43 +0700

    Tao 2 class StudentCreate va StudentUpdate

--- student_app_hieu/models/student.py
+++ student_app_hieu/models/student.py
@@ -1,7 +1,55 @@
-class Student:
-    def __init__(self, id, name, age, gpa):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.gpa = gpa
-        
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+class StudentCreate(BaseModel):
+    id: str 
+    name: str
+    age: int
+    gpa: float 
+    # thêm đk tên không được để trống hay là các khoảng trắng
+    @field_validator('id', 'name')
+    def not_empty(cls, v, info):
+        if v is None or (isinstance(v, str) and not v.strip()):
+            raise ValueError(f"{info.field_name} khong duoc de trong")
+        return v
+    
+    @field_validator('age')
+    def age_valid(cls, v, info):
+        if not (1 <= v <= 60):
+            raise ValueError("Tuoi khong hop le")
+        return v
+    #gpa phải hợp lệ
+    @field_validator('gpa')
+    def gpa_valid(cls, v, info):
+        if not (0.0 <= v <= 10.0):
+            raise ValueError("GPA khong hop le")
+        return v
+    
+
+class StudentUpdate(BaseModel):
+    id: Optional[str] = None
+    name: Optional[str] = None
+    age: Optional[int] = None 
+    gpa: Optional[float] = None 
+
+    @field_validator('id', 'name')
+    def not_empty(cls, v, info):
+        if v is None or (isinstance(v, str) and not v.strip()):
+            raise ValueError(f"{info.field_name} khong duoc de trong")
+        return v
+    
+    @field_validator('age')
+    def age_valid(cls, v, info):
+        if not (1 <= v <= 60):
+            raise ValueError("Tuoi khong hop le")
+        return v
+
+    @field_validator('gpa')
+    def gpa_valid(cls, v, info):
+        if not (0.0 <= v <= 10.0):
+            raise ValueError("GPA khong hop le")
+        return v
+    
+class StudentOut(BaseModel):
+    
+
